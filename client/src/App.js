2a2da1b710d4453ca4ebb9a061b1348760d363dd
Matthew Bell <matthewbell1030@gmail.com>
2020-07-09 12:56:22 -0700

    Added basic structure to 'Footer' component.  Added Footer component to 'App.js'.

--- client/src/App.js
+++ client/src/App.js
@@ -10,26 +10,32 @@ import GalleryCode from "./pages/GalleryCode";
 import GalleryVFX from "./pages/GalleryVFX";
 
 //  Components
+import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 // App
 function App () {
     return (
-        <Router>
-          <div>
-            <Nav />
-            <Switch>
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/gallery_code" component={GalleryCode} />
-              <Route path="/gallery_vfx" component={GalleryVFX} />
-              <Route path="/home" component={Home} />
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </div>
-        </Router>
+        <Jumbotron>
+          <Router>
+            <div>
+              <Nav />
+              <Switch>
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/gallery_code" component={GalleryCode} />
+                <Route path="/gallery_vfx" component={GalleryVFX} />
+                <Route path="/home" component={Home} />
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </div>
+          </Router>
+        </Jumbotron>
+        
+        <Footer></Footer>
     );
 }
 
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer({ children }) {
+    return (
+      <div className="Footer col-md-12">
+        {children}
+      </div>
+    );
+  }
+  
+  export default Footer;
