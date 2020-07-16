669b0fd061593e85e6b4fb3bae4fefc100724564
Matthew Bell <matthewbell1030@gmail.com>
2020-07-16 09:00:19 -0700

    Copied code over from 'home.js' for a starting point, and began customizing.  Removed right column in main body, and updated 'about' header and 'about' buttons.

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,11 +1,61 @@
-//DEPENDENIES
-import React from "react";
+//DEPENDENCIES
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBView,
+  MDBContainer
+} from 'mdbreact';
+
+import '../App.js'
+import './css/home.css';
 
 //PAGE
-const About = () => {
+class About extends React.Component {
+  state = {
+    collapsed: false
+  };
+
+  handleTogglerClick = () => {
+    const { collapsed } = this.state;
+
+    this.setState({
+      collapsed: !collapsed
+    });
+  };
+
+  render() {
+    const { collapsed } = this.state;
+
     return (
-        <div>About Page</div>
+      <div id='minimalistintro'>
+        <MDBView src='https://mdbootstrap.com/img/Photos/Others/img%20%2848%29.jpg'>
+          <MDBMask className='rgba-black-light' />
+          <MDBContainer className='homeContainer d-flex justify-content-center align-items-center'>
+            <MDBRow>
+                {/* Main column will contain about header, text, and calls to action */}
+                <MDBCol md='12' className='mb-4 white-text text-center'>
+                    <h1 className='h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 '>
+                        About Me
+                    </h1>
+                    <hr className='hr-light my-4' />
+                    <h4 className='text-uppercase mb-4 white-text '>
+                        <strong>Web Design and VFX</strong>
+                    </h4>
+                    <p>All the Lorems</p>
+                    <MDBBtn outline color='white'>Contact Me</MDBBtn>
+                    <MDBBtn outline color='white'>Galleries</MDBBtn>
+                    
+                </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </MDBView>
+      </div>
     );
-};
+  }
+}
 
 export default About;
