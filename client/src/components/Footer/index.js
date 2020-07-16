3866f29612aba057b86b69ff83b403083f88bb7a
Matthew Bell <matthewbell1030@gmail.com>
2020-07-16 13:42:31 -0700

    Added image assets for portrait and projects to /src/images

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,26 +7,36 @@ const Footer = () => {
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
+            <h5 className="title">Matthew Bell</h5>
             <p>
-              Here you can use rows and columns here to organize your footer
-              content.
+              email: matthewbell1030@gmail.com
+              <br />
+              phone: 858-922-5440
             </p>
           </MDBCol>
           <MDBCol md="6">
-            <h5 className="title">Links</h5>
+            <h5 className="title text-center">Links</h5>
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Link 1</a>
+              <a href="https://github.com/Mbell42" target="_blank">
+                  <p class="text-center">
+                      Github Profile
+                  </p>
+                </a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 2</a>
+                <a href="https://www.linkedin.com/in/matthew-bell-developer-artist/" target="_blank">
+                  <p class="text-center">
+                      LinkedIn Profile
+                  </p>
+                </a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
+                <a href="https://vimeo.com/matthewgbell" target="_blank">
+                  <p class="text-center">
+                      Vimeo Reel
+                  </p>
+                </a>
               </li>
             </ul>
           </MDBCol>
@@ -34,6 +44,9 @@ const Footer = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
+        <p class="text-center">
+            matthewbell1030@gmail.com<br />phone: 858-922-5440
+          </p>
           &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
         </MDBContainer>
       </div>
