984f685fb37d3e8f199e7e888e9b32b8bc05a96b
Matthew Bell <matthewbell1030@gmail.com>
2020-07-21 07:36:22 -0700

    Added alternate options for SMTP credentials using process.env.  This should allow app to access credentials from config.js when deployed on Heroku.

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.listen(3002);
 
 //nodemailer section
 let transport = {
-  host: creds.SMTP, //provider address
+  host: creds.SMTP || process.env.SMTP_ADDRESS, //provider address
   port: 587,
   secure: false,
   auth: {
-    user: creds.USER,
-    pass: creds.PASS
+    user: creds.USER || process.env.SMTP_USER,
+    pass: creds.PASS || process.env.SMTP_PASS
   },
 };
 
