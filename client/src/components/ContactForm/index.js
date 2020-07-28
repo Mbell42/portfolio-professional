1083206ade14c6770f6941c5958e431ff5cc8750
Matthew Bell <matthewbell1030@gmail.com>
2020-07-27 23:59:17 -0700

    Added Component ContactForm to break out ContactForm from Contact.js page.

---
+++ client/src/components/ContactForm/index.js
@@ -0,0 +1,18 @@
+//DEPENDENCIES
+import React from "react";
+import axios from "axios";
+    //components
+import BgImage from "../components/BgImage";
+import { 
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBIcon,
+    MDBBtn,
+    MDBInput } from "mdbreact";
+    //styles
+import '../pages/css/pages.css';
+
+//COMPONENT
