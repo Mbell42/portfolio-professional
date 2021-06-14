beaded5a8fe1ba7fa2b148270aeca4cdea94c309
Matthew Bell <matthewbell1030@gmail.com>
2021-06-14 12:18:05 -0700

    created 'galleryDesign.js'.  this page is intended to be a gallery page to showcase design-related projects.

---
+++ client/src/pages/galleryDesign.js
@@ -0,0 +1,15 @@
+//DEPENDENCIES
+import React from "react";
+    //components
+import BgImage from "../components/BgImage";
+import { 
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCardBody
+} from "mdbreact";
+    //styles
+import './css/pages.css';
+    //images
+
+//PAGE
