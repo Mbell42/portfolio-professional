25dd843a470470c625a24c3385aac74b7024c208
Matthew Bell <matthewbell1030@gmail.com>
2021-06-14 15:37:22 -0700

    for gallery_design, created gallery header section and projects sections, and added and adapted project templates from the gallery_code page.

--- client/src/pages/galleryDesign.js
+++ client/src/pages/galleryDesign.js
@@ -6,11 +6,13 @@ import {
     MDBContainer,
     MDBRow,
     MDBCol,
+    MDBBtn,
     MDBCardBody
 } from "mdbreact";
     //styles
 import './css/pages.css';
     //images
+import steadFast from "../images/projects/code/steadfast-logo-square.png";
 
 //PAGE
 const GalleryDESIGN = () => {
@@ -27,12 +29,62 @@ const GalleryDESIGN = () => {
                         </h1>
                         <hr className='hr-light my-4' />
                         <h5 className="white-text font-weight-bold w-responsive mx-auto mb-5">
-                            A collection of my past projects, including school projects.  Click the images to view the deployed projects.
+                            A collection of my past projects, including projects for FreeCodeCamp's Responsive Web Design course.  Click the images to view the deployed projects.
                         </h5>
 
                         {/* Projects Section */}
                         <MDBRow className="text-center flex-row">
-
+                        {/* portfolio design project */}
+                            <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+                                <a href="https://steadfast-school.herokuapp.com/" target={"blank"}>
+                                    <img
+                                        src={steadFast}
+                                        alt="portfolio design"
+                                        className="img-fluid rounded z-depth-2 projectImage"
+                                    />
+                                </a>
+                                <MDBCardBody className="pb-0 mb-3">
+                                    <h4 className="font-weight-bold my-2">Portfolio Design</h4>
+                                    <p className="white-text mb-2">
+                                        
+                                    </p>
+                                    <a href="https://github.com/Mbell42/steadfast-school" target={"blank"} className="gitLink"><MDBBtn gradient="purple">Github Link</MDBBtn></a>
+                                </MDBCardBody>
+                            </MDBCol>
+                            {/* survey form project */}
+                            <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+                                <a href="https://steadfast-school.herokuapp.com/" target={"blank"}>
+                                    <img
+                                        src={steadFast}
+                                        alt="survey form design"
+                                        className="img-fluid rounded z-depth-2 projectImage"
+                                    />
+                                </a>
+                                <MDBCardBody className="pb-0 mb-3">
+                                    <h4 className="font-weight-bold my-2">Survey Form Design</h4>
+                                    <p className="white-text mb-2">
+                                        
+                                    </p>
+                                    <a href="https://github.com/Mbell42/steadfast-school" target={"blank"} className="gitLink"><MDBBtn gradient="purple">Github Link</MDBBtn></a>
+                                </MDBCardBody>
+                            </MDBCol>
+                            {/* technical doc-PHP project */}
+                            <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+                                <a href="https://steadfast-school.herokuapp.com/" target={"blank"}>
+                                    <img
+                                        src={steadFast}
+                                        alt="technical document design"
+                                        className="img-fluid rounded z-depth-2 projectImage"
+                                    />
+                                </a>
+                                <MDBCardBody className="pb-0 mb-3">
+                                    <h4 className="font-weight-bold my-2">Technical Doc Design</h4>
+                                    <p className="white-text mb-2">
+                                        
+                                    </p>
+                                    <a href="https://github.com/Mbell42/steadfast-school" target={"blank"} className="gitLink"><MDBBtn gradient="purple">Github Link</MDBBtn></a>
+                                </MDBCardBody>
+                            </MDBCol>
                         </MDBRow>
                         {/* End Projects Section */}
                     </MDBCol>
